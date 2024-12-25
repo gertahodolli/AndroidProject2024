0fa5374a51374ca5439d46acb0ae781247a4f1cb
Endrit Kastrati <endritkastrati8@gmail.com>
2024-12-25 18:10:11 +0100

    Adding the Recipe Details Activity as the main Dashboard of the app

---
+++ app/src/main/java/com/example/androidproject2024/RecipeDetailsActivity.java
@@ -0,0 +1,29 @@
+package com.example.androidproject2024;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+
+        ImageView recipeImageView = findViewById(R.id.recipeImageView);
+        TextView recipeNameTextView = findViewById(R.id.recipeNameTextView);
+        TextView recipeInstructionsTextView = findViewById(R.id.recipeInstructionsTextView);
+
+        // Get data from intent
+        String name = getIntent().getStringExtra("name");
+        int image = getIntent().getIntExtra("img.png", 0);
+        String instructions = getIntent().getStringExtra("instructions");
+
+        // Set data
+        recipeNameTextView.setText(name);
+        recipeImageView.setImageResource(image);
+        recipeInstructionsTextView.setText(instructions);
+    }
